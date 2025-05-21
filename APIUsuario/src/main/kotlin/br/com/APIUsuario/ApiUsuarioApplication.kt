4a0495519c297f99ebcd46b7031b62247bbaa7cf
João Ganança <93109162+Joaogananca26@users.noreply.github.com>
2025-05-21 17:44:43 -0300

    Adicionando spring initializr de maneira certa

---
+++ APIUsuario/src/main/kotlin/br/com/APIUsuario/ApiUsuarioApplication.kt
@@ -0,0 +1,11 @@
+package br.com.APIUsuario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiUsuarioApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiUsuarioApplication>(*args)
+}
---
+++ APIUsuario/src/test/kotlin/br/com/APIUsuario/ApiUsuarioApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.APIUsuario
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiUsuarioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
