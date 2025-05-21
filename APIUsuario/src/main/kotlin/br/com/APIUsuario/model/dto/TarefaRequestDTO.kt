9044e1d520f2563c8297fc3de6631294772aa4fc
joaogananca26 <joaogananca26@gmail.com>
2025-05-21 18:01:39 -0300

    Começando o projeto

---
+++ APIUsuario/src/main/kotlin/br/com/APIUsuario/model/dto/TarefaRequestDTO.kt
@@ -0,0 +1,4 @@
+package br.com.APIUsuario.model.dto
+
+class TarefaRequestDTO {
+}
---
+++ APIUsuario/src/main/kotlin/br/com/APIUsuario/model/entity/Tarefa.kt
@@ -0,0 +1,8 @@
+package br.com.APIUsuario.model.entity
+
+
+data class Tarefa(
+    val idTarefa: String,
+    val titulo: String,
+    val descricao: String
+)
